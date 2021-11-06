7dfbedc0ac45877f8c47edff3e2642f1af83b42f
Jeff Gleed <fensborough@outlook.com>
2021-11-06 16:32:20 +0000

    Removve unwanted text from dcred page and add Wraith font

--- src/pages/dcredPage.js
+++ src/pages/dcredPage.js
@@ -2,13 +2,14 @@ import React, { useRef, useState, useEffect } from "react";
 import Join from "../components/join";
 import Footer from "../components/footer";
 import Navbar from "../components/common/navbar";
+import HeroDcred from "../components/heroDcred";
 
 export default function DcredPage() {
     const [menu, setmenu] = useState(false);
     return (
         <>
             <Navbar menu={menu} setmenu={setmenu} />
-            <h1 class="text-white">$Dcred Page</h1>
+            <HeroDcred />
             <Join />
             <Footer />
         </>
