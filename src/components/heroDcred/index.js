b72c9709e96ec050af8015f062089fb3fa8d148c
Jeff Gleed <fensborough@outlook.com>
2021-11-06 18:50:03 +0000

    Add initial description box into dcred screen

--- src/components/heroDcred/index.js
+++ src/components/heroDcred/index.js
@@ -2,14 +2,16 @@ import React from "react";
 
 function Index() {
     return (
-      <div
-        style={{ backgroundImage: `url(images/dcredbackground.png)` }}
-        className="bg-cover bg-center h-full w-full py-24 md:py-32 lg:py-40 xl:py-60 z-10">
-        <div className="flex flex-col items-center justify-center mx-auto container ">
-          <p className="text-white text-2xl pb-16">
-              $Dcred
-          </p>
-        </div>
+        <div>
+            <div
+            style={{ backgroundImage: `url(images/dcredbackground.png)` }}
+            className="bg-cover bg-center h-full w-full py-24 md:py-32 lg:py-40 xl:py-40 z-10">
+                <div class="flow-root text-white">
+                    <div class="float-right mr-48 h-80 w-1/3 h-96 border-4 border-indigo-600 bg-black">
+                        A complete portal for geeks
+                    </div>
+                </div>
+            </div>
       </div>
   );
 }
