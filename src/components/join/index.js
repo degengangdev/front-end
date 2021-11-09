6f1303e58810a1935efeea057beeeb1ab0c04d7c
Jeff Gleed <fensborough@outlook.com>
2021-11-09 16:56:17 +0000

    Fix png casing

--- src/components/join/index.js
+++ src/components/join/index.js
@@ -4,7 +4,7 @@ import Fade from "react-reveal/Fade";
 function Index() {
   return (
       <div
-        style={{ backgroundImage: `url(images/jointhedegengang.png)` }}
+          style={{ backgroundImage: `url(images/JoinTheDegenGang.png)` }}
         className="bg-cover bg-center h-full w-full py-24 md:py-32 lg:py-20 xl:py-40 z-10"
       >
         <div className="flex flex-col items-center justify-center mx-auto container pl-8 pr-2">
