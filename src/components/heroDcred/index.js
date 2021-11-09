a779c47b6886bf5f944257e08a043a8de5dac3c3
Jeff Gleed <fensborough@outlook.com>
2021-11-09 20:20:16 +0000

    Complete dcred screen

--- src/components/heroDcred/index.js
+++ src/components/heroDcred/index.js
@@ -6,9 +6,9 @@ function Index() {
         <div>
             <div
                 style={{ backgroundImage: `url(images/DcredBackground.png)` }}
-            className="bg-cover bg-center h-full w-full py-8 md:py-10 lg:py-30 xl:py-40 z-10 mb-8">
+            className="bg-cover bg-center h-full w-full py-8 md:py-10 lg:py-30 xl:py-40 z-10 mb-8 pl-2">
                 <div className="flow-root text-white">
-                    <div className="ml-4 md:float-right xl:mr-64 lg:mr-8 md:mr-20 md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:w-1/3 border-2 border-purpleBrand lg:bg-black pt-4 md:max-w-screen-sm mr-4 ">
+                    <div className="ml-4 md:float-right xl:mr-64 lg:mr-8 md:mr-20 md:w-1/2 lg:w-1/2 xl:w-1/3 2xl:w-1/3 border-2 border-purpleBrand md:bg-black pt-4 md:max-w-screen-sm mr-4 ">
                         <div className="flex flex-col items-center justify-center mx-auto container font-robmono p-1 pb-8 pl-4 pr-4 pt-2 text-sm md:text-lg ">
                             <span className="font-wraith">What is $Dcred?</span>
                             <span className="mt-6 xl:mt-12">Degen Street Cred, $DCRED, is our governance token that will give degens the power to</span>
--- src/components/heroStreetCred/index.js
+++ src/components/heroStreetCred/index.js
@@ -5,15 +5,15 @@ function Index() {
         <div>
             <div
                 style={{ backgroundImage: `url(images/DegenStreetCred.png)` }}
-                className="bg-cover bg-center h-full w-full py-4 md:py-10 lg:py-20 xl:py-20 z-10">
-                <div className="flex flex-col items-center justify-center mx-auto container p-1 pb-8 text-white f-f-r pl-8 max-w-screen-md">
+                className="bg-cover bg-center h-full w-full py-4 md:py-10 lg:py-10 xl:py-10 z-10">
+                <div className="flex flex-col items-center justify-center mx-auto container p-1 pb-8 text-white f-f-r pl-8 max-w-screen-sm md:pt-28 md:pb-40">
                     <p className="font-wraith text-lg md:text-2xl">DEGEN STREET CRED</p>
                     <p className="font-wraith text-lg md:text-2xl">$DCRED</p>
-                    <p className="pt-8">As with any gang, the longer you're in, the more you contribute and the more influential you are, you gain street cred.</p>
-                    <p className="pt-8">Degen Street Cred, $DCRED, is our governance token that will give degens the power to influence decisions and decide how the ecosystem should further develop.</p>
+                    <p className="pt-12">As with any gang, the longer you're in, the more you contribute and the more influential you are, you gain street cred.</p>
+                    <p className="pt-12">Degen Street Cred, $DCRED, is our governance token that will give degens the power to influence decisions and decide how the ecosystem should further develop.</p>
                     <img
                         alt=""
-                        className="pt-12"
+                        className="pt-36"
                         src="images/degenganglogocircle.png"
                     />
 
