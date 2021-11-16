e29e360a63887ff94595d94008d6d7e363422137
Jeff Gleed <fensborough@outlook.com>
2021-11-16 12:16:26 +0000

    Add titles to degen types module

--- src/components/degenTypes/index.js
+++ src/components/degenTypes/index.js
@@ -1,54 +1,59 @@
 import React from "react";
 
 function Index() {
-    return (
-        <div style={{ backgroundImage: `url(images/brand-colour-1.png)` }}>
-            <div className="h-auto w-full py-6 z-10 -mt-12" >
-                <div className="flex flex-col items-center justify-center mx-auto container p-1 pb-1 text-white f-f-r">
-                    <div class="grid grid-cols-1 md:grid-cols-3 gap-0  max-w-screen-lg ">
-                        <div >
-                            <img
-                                alt=""
-                                className=""
-                                src="images/DegenGenesis.png"
-                            />
-                            <p className="ml-16 mr-16 -mt-16">
-                                OG Degens. These Degens were here from the start and are the only ones that can
-                                generate 10 $DCRED a day.
+  return (
+    <div style={{ backgroundImage: `url(images/brand-colour-1.png)` }}>
+      <div className="h-auto w-full py-6 z-10 -mt-12" >
+        <div className="flex flex-col items-center justify-center mx-auto container p-1 pb-1 text-white f-f-r">
+          <p className="text-white text-large font-wraith text-2xl text-center">Types of Degen</p>
 
-                                $DCRED can be used to name a Degen, write a lore for them, or even breed a beast...
-                            </p>
-                        </div>
-                        <div >
-                            <img
-                                alt=""
-                                className=""
-                                src="images/PixelDegens.png"
-                            />
-                            <p className="ml-16 mr-16 -mt-16">
-                                Pixel Degens lurk on the
-                                Immutable X platform and like to fuck shit up in the bar.
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-0  max-w-screen-lg pt-8">
+            <div >
+              <p className="text-white font-wraith text-md text-center">Genesis</p>
+              <img
+                alt=""
+                className="-mt-12"
+                src="images/DegenGenesis.png"
+              />
+              <p className="ml-16 mr-16 -mt-16">
+                OG Degens. These Degens were here from the start and are the only ones that can
+                generate 10 $DCRED a day.
 
-                                These angry degens can be used in BAR FIGHTS to earn you $DEGEN for taking names and kicking ass.
-                            </p>
-                        </div>
-                        <div >
-                            <img
-                                alt=""
-                                className=""
-                                src="images/BeastDegens.png"
-                            />
-                            <p className="ml-16 mr-16 -mt-16">
-                                A science experiment that went horribly wrong and now we have beasts to contend with...
-
-                                There is still much to learn of these creatures and their powers.
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                $DCRED can be used to name a Degen, write a lore for them, or even breed a beast...
+              </p>
             </div>
+            <div >
+              <p className="text-white font-wraith text-md text-center">Pixel Degens</p>
+              <img
+                alt=""
+                className="-mt-12"
+                src="images/PixelDegens.png"
+              />
+              <p className="ml-16 mr-16 -mt-16">
+                Pixel Degens lurk on the
+                Immutable X platform and like to fuck shit up in the bar.
+
+                These angry degens can be used in BAR FIGHTS to earn you $DEGEN for taking names and kicking ass.
+              </p>
+            </div>
+            <div >
+              <p className="text-white font-wraith text-md text-center">Beast Degens</p>
+              <img
+                alt=""
+                className="-mt-12"
+                src="images/BeastDegens.png"
+              />
+              <p className="ml-16 mr-16 -mt-16">
+                A science experiment that went horribly wrong and now we have beasts to contend with...
+
+                There is still much to learn of these creatures and their powers.
+
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
