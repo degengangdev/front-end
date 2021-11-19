ebf3fe22ec5c40a1c8cb27089482d1bb9be7d801
Jeff Gleed <fensborough@outlook.com>
2021-11-19 12:20:30 +0000

    Switch to production settings

--- src/components/ImmutableX/imxConfig.js
+++ src/components/ImmutableX/imxConfig.js
@@ -1,7 +1,7 @@
 //ropsten
-export const linkAddress = 'https://link.ropsten.x.immutable.com';
-export const baseURL = 'https://dgx-pxl-imx-microservice.herokuapp.com/';
+//export const linkAddress = 'https://link.ropsten.x.immutable.com';
+//export const baseURL = 'https://dgx-pxl-imx-microservice.herokuapp.com/';
 
 // //mainnet
- //export const linkAddress = 'https://link.x.immutable.com';
- //export const baseURL = 'https://dgx-pxl-imx-microservice.herokuapp.com/';
+ export const linkAddress = 'https://link.x.immutable.com';
+ export const baseURL = 'https://dgx-pxl-imx-microservice.herokuapp.com/';
