84a77cf08bc17dcdc8b59606728c56900b13f804
Jeff Gleed <fensborough@outlook.com>
2021-11-27 21:17:56 +0000

    Add unsold degens section

--- src/pages/claimPage.js
+++ src/pages/claimPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ClaimPageHero from "../components/claimPage/claimPageHero";
 import ClaimPageLitePaper from "../components/claimPage/claimPageLitePaper";
+import UnsoldDegens from "../components/claimPage/unsoldDegens";
 import Join from "../components/join";
 import Footer from "../components/footer";
 
@@ -8,6 +9,7 @@ export default function ClaimPage() {
   return (
     <>
       <ClaimPageHero />
+      <UnsoldDegens />
       <ClaimPageLitePaper />
       <Join />
       <Footer />
