e0e98a9514f96ab33dc082b3dba3181b9d9f3b33
Jeff Gleed <fensborough@outlook.com>
2021-12-01 14:48:43 +0000

    Fix soldout layout

--- src/components/soldOut/index.js
+++ src/components/soldOut/index.js
@@ -1,38 +1,38 @@
 import React from "react";
 
 function Index() {
-    return (
-        <div id="soldOut">
-            <div className="h-auto w-full pt-6 z-10 px-8">
-                <div className="flex flex-col md:flex-row items-center justify-center mx-auto container">
-                  <img
-                    alt=""
-                    className="mt-6 hidden md:block lg:hidden 2xl:block"
-                    src="images/SoldOutLeftMullet.png"
-                  />
-                  <img
-                    alt=""
-              className="mt-8 md:hidden lg:block"
-                    src="images/SoldOutLeftTwo.png"
-                  />
-                  <img
-                      alt=""
-                      className="-mt-6 ml-8"
-                      src="images/sold-out-smaller.gif"
-                  />
-                  <img
-                      alt=""
-                      className="ml-6 md:hidden lg:block"
-                      src="images/SoldOutRightTwo.png"
-                  />
-                  <img
-                      alt=""
-              className="-ml-6 hidden md:block lg:hidden 2xl:block"
-                      src="images/SoldOutRightSingle.png"
-                  />
-                </div>
-            </div>
+  return (
+    <div id="soldOut">
+      <div className="h-auto w-full pt-6 z-10 px-8">
+        <div className="flex flex-col md:flex-row items-center justify-center mx-auto">
+          <img
+            alt=""
+            className="mt-6 hidden md:block lg:hidden 2xl:block"
+            src="images/SoldOutLeftMullet.png"
+          />
+          <img
+            alt=""
+            className="mt-8 md:hidden lg:block"
+            src="images/SoldOutLeftTwo.png"
+          />
+          <img
+            alt=""
+            className="mt-6 ml-8"
+            src="images/sold-out-smaller.gif"
+          />
+          <img
+            alt=""
+            className="ml-6 md:hidden lg:block"
+            src="images/SoldOutRightTwo.png"
+          />
+          <img
+            alt=""
+            className="-ml-6 hidden md:block lg:hidden 2xl:block"
+            src="images/SoldOutRightSingle.png"
+          />
+        </div>
       </div>
+    </div>
   );
 }
 
