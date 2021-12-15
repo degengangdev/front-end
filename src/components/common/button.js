ccca97fc9c34e48f5c668c02ee3a66daf5c31648
Jeff Gleed <fensborough@outlook.com>
2021-12-15 15:20:10 +0000

    Tidy up code layout

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -6,9 +6,9 @@ function button({ title, onClickhandler = null, tailwind = null }) {
   return (
     <button
       className={tailwind + " font-wraith focus:outline-none px-8 py-3 text-xs md:text-base md:py-11px bg-purpleBrand text-white shadow-btn hover:shadow-none transition-all duration-300 ease-in-out hover:scale-110 hover:bg-skyBlue transform"}
-      onClick={e=>onClickhandler && onClickhandler()}
+      onClick={e => onClickhandler && onClickhandler()}
     >
-          <p className="-mt-1">{title}</p>
+      <p className="-mt-1">{title}</p>
     </button>
   );
 }
